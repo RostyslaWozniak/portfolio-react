cd4bf8f9776f871b9221d0302199cd96d64a6cf5
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-23 18:21:42 +0100

    fix main background video

--- src/Section/MainSection/HomeBackground.tsx
+++ src/Section/MainSection/HomeBackground.tsx
@@ -1,4 +1,5 @@
-import video from '../../asets/video/video.webm';
+import video from '../../asets/video/video-webm.webm';
+import image from '../../asets/images/background/loader/loader-bg.png';
 
 const HomeBackground = () => {
     return ( 
@@ -9,7 +10,7 @@ const HomeBackground = () => {
           muted 
           className="h-xl-screen fixed top-0 left-0 w-screen object-cover brightness-[60%] z-[-10]">
           <source 
-            src={video} 
+            src={video || image} 
             type="video/mp4" 
           />
           Your browser does not support the video tag.
