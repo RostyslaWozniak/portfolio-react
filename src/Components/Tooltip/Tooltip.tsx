235c4886928dbc226f55d80a571d743f5ced8ece
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-23 17:41:53 +0100

    change display none on phones

--- src/Components/Tooltip/Tooltip.tsx
+++ src/Components/Tooltip/Tooltip.tsx
@@ -7,7 +7,7 @@ const Tooltip = ({ value } : TooltipProps) => {
     return ( 
         <div 
             style={{animation: '.3s linear show-tooltip'}}
-            className="absolute top-[-50px] px-10 py-1 bg-background border rounded-lg text-center shadow-md z-[20] duration-300">
+            className="absolute hidden top-[-50px] px-10 py-1 bg-background border rounded-lg text-center shadow-md z-[20] duration-300 tablet:block">
             {value}
         </div>
      );
