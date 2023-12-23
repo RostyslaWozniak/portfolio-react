1eac167e8afbc6e929c2c5812ea23eb908a0e6e3
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-23 18:39:55 +0100

    fix main bg video

--- src/Section/MainSection/HomeBackground.tsx
+++ src/Section/MainSection/HomeBackground.tsx
@@ -1,16 +1,20 @@
 import video from '../../asets/video/video-webm.webm';
-import image from '../../asets/images/background/loader/loader-bg.png';
+import image from '../../asets/video/image.png';
 
 const HomeBackground = () => {
     return ( 
-
         <video 
           autoPlay 
           loop 
           muted 
-          className="h-xl-screen fixed top-0 left-0 w-screen object-cover brightness-[60%] z-[-10]">
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+          }}
+          className="h-xl-screen fixed top-0 left-0 w-screen object-cover brightness-[60%] z-[-10]"
+          >
           <source 
-            src={video || image} 
+            src={video} 
             type="video/mp4" 
           />
           Your browser does not support the video tag.
