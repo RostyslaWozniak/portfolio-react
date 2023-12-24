e92477a0f93ca2e4c34fbd60e6858a6f8e8c547e
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-24 10:13:47 +0100

    refactor: add new arrow

--- src/Components/ArrowAnimations/MainArrow.tsx
+++ src/Components/ArrowAnimations/MainArrow.tsx
@@ -1,13 +1,14 @@
 import { useAppContext } from '../../Context/AppContext';
-import arrow from '../../asets/images/animations/arrow1.svg';
 const MainArrow = () => {
     const { hasHeaderBackground } = useAppContext();
     return ( 
         <>
             {!hasHeaderBackground
             &&
-            <div className="w-screen flex justify-center scale-[.7] translate-x-[-5%]">
-                <img className="" src={arrow}/>
+            <div id="main-arrow-container" className="border">
+                <div className="first"></div>
+                <div className="second"></div>
+                <div className="third"></div>
             </div>}
         </>
      );
--- src/Section/MainSection/Home.tsx
+++ src/Section/MainSection/Home.tsx
@@ -2,15 +2,15 @@ import MainArrow from '../../Components/ArrowAnimations/MainArrow';
 import './css/style.css';
 const Home = () => {
   return (
-  <section className="relative w-sectionWidth h-screen mx-auto "> 
+  <section className="relative w-sectionWidth h-screen mx-auto"> 
     <h1 
     style={{animation: '4s 2s linear h1-animation'}}
-    className="fixed top-[40%] text-[min(4rem,6vw)] font-bold tracking-widest leading-[min(6rem,10vw)] z-[-1]">
+    className="fixed bottom-[25vh] text-[min(4rem,8vw)] laptop:font-bold laptop:tracking-widest leading-[min(6rem,15vw)] z-[-1]">
       Hi, <br/>
       My Name is Rostik<br/>
       and I'm Web Developer.<br/>
     </h1>
-    <a href="#about" className="fixed bottom-0 tablet:hidden">
+    <a href="#about" className="fixed w-full h-[200px] left-0 bottom-0 tablet:hidden">
       <MainArrow/>
     </a>
   </section>
