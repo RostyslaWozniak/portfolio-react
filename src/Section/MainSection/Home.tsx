7538795391cb6b30463b4b0dacf68baba36bcc1a
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-24 11:26:01 +0100

    fix: arrow a to div

--- src/Section/MainSection/Home.tsx
+++ src/Section/MainSection/Home.tsx
@@ -1,18 +1,26 @@
+import { useEffect, useState } from 'react';
 import MainArrow from '../../Components/ArrowAnimations/MainArrow';
 import './css/style.css';
 const Home = () => {
+  const [showHeadingDelay, setShowHeadingDelay] = useState(2);
+  const tabletWidth = 650;
+  useEffect(() => {
+    //Logo
+    
+    if(window.innerWidth < tabletWidth) setShowHeadingDelay(0);
+}, []);
   return (
   <section className="relative w-sectionWidth h-screen mx-auto"> 
     <h1 
-    style={{animation: '4s 2s linear h1-animation'}}
+    style={{animation: `4s ${showHeadingDelay}s linear h1-animation`}}
     className="fixed bottom-[25vh] text-[min(4rem,8vw)] laptop:font-bold laptop:tracking-widest leading-[min(6rem,15vw)] z-[-1]">
       Hi, <br/>
       My Name is Rostik<br/>
       and I'm Web Developer.<br/>
     </h1>
-    <a href="#about" className="fixed w-full h-[200px] left-0 bottom-0 tablet:hidden">
+    <div className="fixed w-full h-[200px] left-0 bottom-0 laptop:hidden">
       <MainArrow/>
-    </a>
+    </div>
   </section>
   );
 };
