8d31fa1c10db034a620776d0183c815277c0b103
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-26 19:43:53 +0100

    style: add text

--- src/Section/AboutSection/About.tsx
+++ src/Section/AboutSection/About.tsx
@@ -9,12 +9,11 @@ const About = () => {
         className="relative bg-primary bg-no-repeat text-font shadow-primary-shadow ">
         <div className="w-sectionWidth mx-auto flex flex-col gap-lg pt-[200px] ">
             <div className="flex flex-col laptop:flex-row gap-lg">
-                <div className="w-[min(600px,100%)] flex flex-col gap-md p-10 text-justify mx-auto border rounded-lg backdrop-blur-md scale-[.98] shadow-sm hover:shadow-lg duration-300 hover:scale-[1]">
+                <div className="w-[min(600px,100%)] flex flex-col gap-[20px] p-10 text-justify mx-auto border rounded-lg backdrop-blur-md scale-[.98] shadow-sm hover:shadow-lg duration-300 hover:scale-[1]">
                     
-                    <h1 className="text-[2rem] text-center z-[1]">ABOUT</h1>
-                    <p className="z-[1]">Hi, welcome to my profile. As a passionate freelance Frontend JavaScript Developer, I bring expertise in creating dynamic and user-friendly interfaces using the powerful combination of React for the frontend, Node.js and Express for server-side logic, and MongoDB for seamless database integration.</p>
-                        
-                    <p>Database Integration with MongoDB: As a frontend developer, I understand the importance of seamless data management. With MongoDB, I ensure your application's data is stored and retrieved efficiently, providing a solid foundation for dynamic and data-driven web applications.</p>
+                    <h1 className="text-[2rem] text-center">ABOUT</h1>
+                    <p className="text-[1.1rem] indent-10">Welcome to my portfolio, where I showcase a collection of innovative web projects designed to leave a lasting impression.  My role is mainly based on bringing Web design to life and visually appealing user experience.  As a Front-end Developer, my mission is to provide users  with an experience that is not only functional but also enjoyable.</p> 
+                    <p className="text-[1.1rem] indent-10 ">My days are filled with lines of code that transform static design into interactive, responsive, and user-friendly interfaces. I see challenges as opportunities for growth. Each bug I encounter is a puzzle to solve. Every project is a chance to refine my skill and expand my knowledge.</p>
                 </div>
                 <div className="w-[min(500px,100%)] flex flex-col justify-around py-5 gap-sm mx-auto">
                     <h2 className="text-[1.8rem] text-center">Languages</h2>
--- src/Section/ProjectsSection/Projects.tsx
+++ src/Section/ProjectsSection/Projects.tsx
@@ -6,10 +6,10 @@ const Projects = () => {
     <section id="projects" className="py-80">
         <div className="w-sectionWidth m-auto flex flex-col laptop:flex-row items-center justify-between gap-sm">
             <div className="w-[min(400px,100%)] h-[550px] flex flex-col justify-between">
-                <h2 className="text-[2rem] text-center">PROJECTS</h2>
-                <div className="grid py-5 px-10 border-2 backdrop-blur-sm rounded-lg backdrop-brightness-75 scale-[.98] shadow-sm-light gap-sm hover:shadow-lg-light hover:scale-[1] duration-300">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam expedita beatae rerum temporibus rem voluptatem facere atque reiciendis! Soluta consectetur sunt ea deleniti ab illo eaque est error corrupti facere.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam expedita beatae rerum temporibus rem voluptatem facere atque reiciendis! Soluta consectetur sunt ea deleniti ab illo eaque est error corrupti facere.</p>
+                <h2 className="text-[2rem] text-center my-20">PROJECTS</h2>
+                <div className="grid  py-5 px-10 border-2 backdrop-blur-sm rounded-lg backdrop-brightness-50 scale-[.98] shadow-sm-light gap-sm hover:shadow-lg-light hover:scale-[1] duration-300">
+                    <p className="text-[1.2rem] indent-10  text-justify">Explore the portfolio to discover content-rich websites that go beyond the ordinary, offering a glimpse into the future of my web skills. Each project is bringing creativity, functionality, and a passion for delivering exceptional digital experiences. 
+                    You can check out my websites on <a href="https://github.com/RostyslaWozniak" className="bold underline">GitHub</a>. Enjoy the graphics!</p>
                 </div>
             </div>
             <div className="grid grid-cols-1 tablet:grid-cols-2 gap-sm place-items-center">
