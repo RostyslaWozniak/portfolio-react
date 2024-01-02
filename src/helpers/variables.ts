dd19f1df4fff4699b7bf90decea8876584b79c41
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-01-02 18:31:44 +0100

    add new file variables

---
+++ src/helpers/variables.ts
@@ -0,0 +1,7 @@
+export const primary = '#e1e8ea';
+export const secondary = '#a4c6c4';
+export const font = '#36474a';
+export const background = '#ffffff';
+
+export const tabletWidth = 650;
+export const laptopWidth = 1000;
